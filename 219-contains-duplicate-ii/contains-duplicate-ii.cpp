6abd2f2087b25a60ae5b3aa020d09208f0223422
snehabhat <bhatsneha83@gmail.com>
2026-03-29 11:07:31 +0530

    Time: 71 ms (76.78%) | Memory: 93.8 MB (77.23%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,22 +1,22 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-
-        unordered_map<int, int> mp;  // value -> last index
+        unordered_set<int> window;
 
         for (int i = 0; i < nums.size(); i++) {
-
-            // agar element pehle se hashmap mein hai
-            if (mp.find(nums[i]) != mp.end()) {
-
-                // index difference check karo
-                if (abs(i - mp[nums[i]]) <= k) {
-                    return true;
-                }
+            if (window.count(nums[i])) {
+                return true;
             }
 
-            // har baar current index update karo
-            mp[nums[i]] = i;
+            window.insert(nums[i]);
+
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
         }
 
         return false;
